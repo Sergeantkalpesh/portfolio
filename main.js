df2fc25ded213401fca19708be990421ba5db917
Sergeantkalpesh <ghanwatkalpesh4545@gmail.com>
2024-09-16 11:06:55 +0530

    Add various codes

--- main.js
+++ main.js
@@ -27,15 +27,81 @@ function myMenuFunction(){
         navHeader.style.lineHeight ="90px";
 
     }
-  }
+}
 
 
 /*-----TYPING EFFECT-----*/
-
-/*-----SCROLL REVEAL ANIMATION -------*/
-
+var typingEffect = new Typed(".typedText",{
+    strings : ["Designer","Cloud Developer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+})
+/*---##--SCROLL REVEAL ANIMATION ---##----*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
 /*----HOME-----*/
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
 
 /* -----PROJECT BOX ------ */
-
+sr.reveal('.project-box',{interval: 200})
 /*-------HEADING---------- */
+sr.reveal('.top-header',{})
+
+/*---------------ABOUT INFO & CONTACT INFO---------------------- */
+const srleft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srleft.reveal('.about-info',{delay: 100})
+srleft.reveal('.contact-info',{delay: 100})
+
+/*--------------ABOUT SKILLS & FORM BOX-------------------------------------------- */
+const srRight =  ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+/*-----------CHANGE ACTIVE LINK--------------------------------- */
+
+const secitons = document.querySelectorAll('section[id')
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    secitons.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+        }
+
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
